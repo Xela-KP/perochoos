5f6a19199bb6357b3df6668ddc1d43de510651b7
Alexander King Perocho <perocho.xela@gmail.com>
2024-04-29 00:05:05 -0400

    fixed ProcessConsumer attempting SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import WindowManager from '../components/system/WindowManager';
 import { ProcessProvider } from '../contexts/processes';
-// import Image from 'next/image';
 export default function Home() {
     return (
         <ProcessProvider>
--- src/contexts/processes.tsx
+++ src/contexts/processes.tsx
@@ -1,17 +1,19 @@
+'use client';
 import React, { createContext, useState } from 'react';
 import processDirectory from '../utils/processDirectory';
-import { ScriptProps } from 'next/script';
 import type { ProcessContextState } from '../types/contexts/processes';
 
 export const ProcessContext = createContext<ProcessContextState>({
     processes: {},
 });
 export const ProcessConsumer = ProcessContext.Consumer;
-export const ProcessProvider: React.FC<ScriptProps> = ({ children }) => {
+export const ProcessProvider = ({ children }) => {
     const [processes] = useState(processDirectory);
     return (
-        <ProcessContext.Provider value={{ processes }}>
-            {children}
-        </ProcessContext.Provider>
+        <>
+            <ProcessContext.Provider value={{ processes }}>
+                {children}
+            </ProcessContext.Provider>
+        </>
     );
 };
