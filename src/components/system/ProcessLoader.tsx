92cdadcfe03ea80e6bcb041b889cd8239669b6e9
Alexander King Perocho <perocho.xela@gmail.com>
2024-05-01 00:41:11 -0400

    added render process component

--- src/components/system/ProcessLoader.tsx
+++ src/components/system/ProcessLoader.tsx
@@ -1,21 +1,28 @@
 'use client';
-import React from 'react';
+import dynamic from 'next/dynamic';
 import { ProcessConsumer } from '../../contexts/process';
-import Window from './Window';
+// import Window from './Window';
+import { Process } from '../../types/contexts/process';
+
+const Window = dynamic(() => import('../system/Window'));
+
+const RenderProcess = ({ Component, hasWindow }: Process) =>
+    hasWindow ? (
+        <Window>
+            <Component />
+        </Window>
+    ) : (
+        <Component />
+    );
 
 const ProcessLoader = () => {
     return (
         <ProcessConsumer>
             {({ processes }) =>
                 Object.entries(processes).map(
-                    ([pid, { Component, hasWindow }]) =>
-                        hasWindow ? (
-                            <Window key={pid}>
-                                <Component />
-                            </Window>
-                        ) : (
-                            <Component key={pid} />
-                        )
+                    ([pid, process]: [pid: string, process: Process]) => (
+                        <RenderProcess key={pid} {...process} />
+                    )
                 )
             }
         </ProcessConsumer>
